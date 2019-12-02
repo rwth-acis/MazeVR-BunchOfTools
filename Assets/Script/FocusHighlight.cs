b2feb60835719602200effb26db1e86d1abce22d
JulianStaab <julianstaab@web.de>
2019-12-02 20:40:58 +0100

    FocusHighlight updated

---
+++ Assets/Script/FocusHighlight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class FocusHighlight : MonoBehaviour, IMixedRealityFocusHandler
+{
+    Renderer rend;
+
+    public Color defaultColor = Color.green;
+    public Color highlightColor = Color.red;
+
+    private void Awake()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rend.material.color = defaultColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        rend.material.color = highlightColor;
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        rend.material.color = defaultColor;
+    }
+}
---
+++ Assets/TiltMazeVR/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Tooltip("The doors in the maze")]
+    public DoorController[] doors;
+
+    /// <summary>
+    /// Resets every game element to its starting state so that a fresh game can be started again
+    /// </summary>
+    public void ResetGame()
+    {
+        foreach (DoorController door in doors)
+        {
+            door.DoorOpen = false;
+        }
+    }
+}
